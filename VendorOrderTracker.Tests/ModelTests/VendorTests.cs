2ec0c07b79fee2214a59941e1444805d2ba58ca3
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-02 11:46:23 -0800

    passed test for getting description

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -11,7 +11,7 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Doggo");
+      Vendor newVendor = new Vendor("Doggo", "Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
@@ -19,7 +19,7 @@ namespace VendorOrderTracker.Tests
     public void GetVendorName_ReturnsVendorName_String()
     {
       string name = "Bunker";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "Description");
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
     }
@@ -28,11 +28,20 @@ namespace VendorOrderTracker.Tests
     public void SetVendorName_SetsValueOfVendorName_String()
     {
       string name = "Bunker";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "Description");
       string updatedName = "Miss Patty's";
       newVendor.Name = updatedName;
       string result = newVendor.Name;
       Assert.AreEqual(updatedName, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnDescriptionofVendor_String()
+    {
+      string description = "Miss Patty's coffee shop";
+      Vendor newVendor = new Vendor("Miss Patty's", description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
