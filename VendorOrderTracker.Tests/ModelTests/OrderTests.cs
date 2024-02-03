aa556b371f8e5b08ae255b34ca52690ceed30431
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-03 15:27:48 -0800

    Get title of order

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,17 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderConstuctor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("Cookie Order");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnsOrderTitle_String()
+    {
+      string title = "Cookie Order";
+      Order newOrder = new Order(title);
+      string result = newOrder.Title;
+      Assert.AreEqual(title, result);
     }
 
   }
