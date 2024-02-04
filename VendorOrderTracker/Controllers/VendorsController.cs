ebbf4d67857abafb8f2136ef5795f6adb03c3b71
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-04 11:53:15 -0800

    Index page is working with button to list of vendors. New vendor create not working

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using VendorOrderTracker.Models;
+using System.Collections.Generic;
 
 namespace VendorOrderTracker.Controllers
 {
   public class VendorsController : Controller
   {
+
     [HttpGet("/vendors")]
-    public ActionResult Index();
+    public ActionResult Index()
     {
+
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
@@ -16,7 +18,52 @@ namespace VendorOrderTracker.Controllers
     [HttpGet("/vendors/new")]
     public ActionResult New()
     {
-      return view();
+      return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor newVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
+
+    // [HttpGet("/vendors/{id}")]
+    // public ActionResult Details(int id)
+    // {
+    //   Pet foundPet = Pet.Find(id);
+    //   return View(foundPet);
+    // }
+
+    // [HttpPost("/vendors/{id}")]
+    // public ActionResult HandlePetAction(int id, string action)
+    // {
+    //   Pet pet = Pet.Find(id);
+
+    //   if (pet == null)
+    //   {
+    //     return NotFound();
+    //   }
+
+    //   switch (action)
+    //   {
+    //     case "attention":
+    //       pet.Attention();
+    //       break;
+    //     case "feed":
+    //       pet.Feed();
+    //       break;
+    //     case "rest":
+    //       pet.Rest();
+    //       break;
+
+    //     default:
+    //       return BadRequest("Invalid action");
+    //   }
+
+    //   return RedirectToAction("Details", new { id = pet.Id });
+    // }
+
+
   }
 }
